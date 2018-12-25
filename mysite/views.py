c15a05b3835a72889f9bfca5196118f65dd07904
JAY <johnjohnsonokah@gmail.com>
2018-12-25 04:16:34 +0100

    Specifying labels and others

--- mysite/views.py
+++ mysite/views.py
@@ -34,8 +34,12 @@ def contact(request):
                 cd['message'],
                 cd.get('email', 'noreply@example.com'),
                 ['siteowner@example.com'],
+                # cd.get('email', ['noreply@example.com](mailto:'noreply%40example.com)'),
+                # [['siteowner@example.com](mailto:'siteowner%40example.com)'],
             )
             return HttpResponseRedirect('/contact/thanks/')
     else:
-        form = ContactForm()
+        form = ContactForm(
+            initial={'subject': 'I love your site!'}
+        )
     return render(request, 'contact_form.html', {'form': form})
